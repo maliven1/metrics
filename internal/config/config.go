ab7abe9a1055973ea844cc7d27bcd0351d896ae4
Li Den <malivenlipton@gmail.com>
2025-10-01 17:39:50 +0300

    add metrics file

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,6 +11,9 @@ var (
 	flagAgentRunAddr  string
 	pollInterval      int
 	reportInterval    int
+	storeInterval     int
+	fileStoragePath   string
+	restore           bool
 )
 
 type AgentConfig struct {
@@ -20,11 +23,17 @@ type AgentConfig struct {
 }
 
 type ServerConfig struct {
-	Address string `env:"ADDRESS"`
+	Address         string `env:"ADDRESS"`
+	StoreInterval   int    `env:STORE_INTERVAL`
+	FileStoragePath string `env:FILE_STORAGE_PATH`
+	Restore         bool   `env:RESTORE`
 }
 
 func ParseServerFlags() {
 	flag.StringVar(&flagServerRunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.IntVar(&storeInterval, "i", 300, "the time interval after which the current server readings are saved")
+	flag.StringVar(&fileStoragePath, "f", "history", "path to the file where the current values are saved")
+	flag.BoolVar(&restore, "r", false, "determines whether previously saved values from the specified file should be loaded when the server starts")
 	flag.Parse()
 }
 func ParseAgentFlags() {
@@ -43,7 +52,15 @@ func NewEnvServerConfig() *ServerConfig {
 	if cfg.Address == "" {
 		cfg.Address = flagServerRunAddr
 	}
-
+	if cfg.StoreInterval == 0 {
+		cfg.StoreInterval = storeInterval
+	}
+	if cfg.FileStoragePath == "" {
+		cfg.FileStoragePath = fileStoragePath
+	}
+	if !cfg.Restore {
+		cfg.Restore = restore
+	}
 	return &cfg
 
 }
