2aac6af475d4fb5977a25f4f810472f8ed2c0f91
Li Den <malivenlipton@gmail.com>
2025-10-08 15:55:59 +0300

    add migrations

--- migrations/000001_create_metrics_table.up.sql
+++ migrations/000001_create_metrics_table.up.sql
@@ -1,5 +1,15 @@
--- migrations/000001_create_metrics_table.down.sql
--- Откат создания таблицы фильмов
-DROP INDEX IF EXISTS idx_gauge_name;
-DROP INDEX IF EXISTS idx_count_value;
-DROP TABLE IF EXISTS metrics;
+-- migrations/000001_create_metrics_table.up.sql
+-- Создание таблицы фильмов
+CREATE TABLE metrics (
+    id SERIAL PRIMARY KEY,
+    gauge_name varchar(255),
+    gauge_value double precision NOT NULL,
+    count_name varchar(255),
+    count_value INTEGER NOT NULL
+); 
+
+-- Базовый индекс для поиска по названию
+CREATE INDEX idx_gauge_name ON gauge_name(name);
+
+-- Индекс для поиска по году
+CREATE INDEX idx_count_value ON count_value(year);
